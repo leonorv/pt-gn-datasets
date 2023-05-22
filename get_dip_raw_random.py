7f464426b57569e421ef70b9f41969efdfc51d01
leonorv <leonor.veloso@tecnico.ulisboa.pt>
2023-05-22 13:01:20 +0100

    reduced dip dataset to modern texts

--- get_dip_raw_random.py
+++ get_dip_raw_random.py
@@ -17,12 +17,14 @@ def get_all_nonempty_lines(file_paths):
         all_lines.extend(get_nonempty_lines(file_path))
     return all_lines
 
-file_paths = glob.glob("./dip/*")
+#file_paths = glob.glob("./dip/*")
+file_paths = ["./dip/obra000.txt", "./dip/obra005.txt", "./dip/obra007.txt", "./dip/obra019.txt", "./dip/obra024.txt", "./dip/obra039.txt", "./dip/obra049.txt", "./dip/obra056.txt", "./dip/obra065.txt", "./dip/obra078.txt", "./dip/obra086.txt", "./dip/obra090.txt", "./dip/obra091.txt", "./dip/obra093.txt","./dip/obra096.txt" ]
 all_lines = get_all_nonempty_lines(file_paths)
 total_lines = len(all_lines)
+print(total_lines)
 
 line_numbers = random.sample(range(total_lines), 20000)
 random_lines = [all_lines[i] for i in line_numbers]
 
-with open('dip_raw_20000.txt', 'w') as f:
+with open('./dip/dip_raw_20000.txt', 'w') as f:
     f.write('\n'.join(random_lines))
--- get_sentences.py
+++ get_sentences.py
@@ -6,8 +6,10 @@ This script shuffles the obtained lines
 import random
 from nltk import tokenize
 
-new = open("./publico/publico_raw_sentences.txt", "w+")
-with open("./publico/publico94.txt") as f:
+#new = open("./publico/publico_raw_sentences.txt", "w+")
+#with open("./publico/publico94.txt") as f:
+new = open("./dip/dip_raw_sentences.txt", "w+")
+with open("./dip/dip_raw_random_20000.txt") as f:
         lines = f.readlines()
         random.shuffle(lines)
 
